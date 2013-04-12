427f222a22d69db9544dedd87e56a7b8d6b9ce69
Andres Junge <ajunge@totexa.cl>
2013-04-12 20:01:50 -0300

    'comments'

--- server.groovy
+++ server.groovy
@@ -1,1 +1,3 @@
-//
+//placeholder for heroku
+//TODO: must fork https://github.com/tomaslin/heroku-buildpack-vertx-jdk7 not to need this file
+//AS READ IN: http://fbflex.wordpress.com/2012/05/02/running-vert-x-applications-on-heroku/
